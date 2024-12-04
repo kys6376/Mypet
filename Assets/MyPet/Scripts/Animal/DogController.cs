0772a94cb46af8f5bbb24a76541e4db356adab69
DESKTOP-52351T8\Admin <kts6376@naver.com>
2024-12-04 10:11:47 +0900

    1. dog 추가

---
+++ Assets/MyPet/Scripts/Animal/DogController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Mypet.AI
+{ 
+public class DogController : AnimalController
+    {
+        protected override void Start()
+        {
+            base.Start();
+
+            //dog와 관련된 상태 추가 등록
+            stateMachine.AddState(new DrinkState());
+            stateMachine.AddState(new SitState());
+            stateMachine.AddState(new DogIdleState());
+        }
+    }
+}
